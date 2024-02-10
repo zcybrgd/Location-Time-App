5efb2f264222d27f04edee04586cf87731af6ac7
zcybrgd <zainebb7k9h1@gmail.com>
2024-02-10 08:57:24 +0000

    learnt about stless widgets, hot reload, Text widget props such as fonts & colors

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,38 @@ import 'package:flutter/material.dart';
 // that widget will allow us to use google material design features, its a wrapper [the root] of the rest of our widgets inside the app
 void main() => runApp(MaterialApp(
   // propreties
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('Location Time App'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('This Is A Flutter Bootcamp'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: null,
-      child: Text('Click'),
-    ),
-  ),
+  home: Home()
 ));
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+  // the hot reload to not to re-build the whole app, just where the code changed
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Location Time App'),
+        centerTitle: true,
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: Center(
+        child: Text(
+          'This Is A Flutter Bootcamp',
+          style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.purple[500],
+              fontFamily: 'Outfit'
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.purple[200],
+      ),
+    );
+  }
+}
