20ba567681ce21f66672abac65c70a9ac6a6e20a
zcybrgd <zainebb7k9h1@gmail.com>
2024-02-14 22:40:00 +0100

    added a couple of things

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,39 +14,58 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     // when we recieve the arguments
     data = ModalRoute.of(context)?.settings.arguments as Map;
+
+    String bgImage = data['isDayTime'] ? 'daytime.jpeg' : 'nighttime.jpeg';
     return Scaffold(
       body: 
       SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 120.0, 0,0),
-            child: Column(
-              children: <Widget>[
-                TextButton.icon(
-                  // push to another route
-                    onPressed: (){
-                      Navigator.pushNamed(context, '/locations');
-                    },
-                    icon: Icon(Icons.edit_location_alt),
-                    label: Text('Edit Location')),
-                SizedBox(height: 20.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(data['location'],
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      letterSpacing: 2.0,
-                      fontFamily: 'Outfit'
-                    )),
-                  ],
-                ),
-                SizedBox(height: 20.0),
-                Text(data['time'],
-                style: TextStyle(
-                  fontSize: 66.0,
-                  fontFamily: 'Outfit'
-                ),),
-              ],
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/$bgImage'),
+                  fit: BoxFit.cover ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 120.0, 0,0),
+              child: Column(
+                children: <Widget>[
+                  TextButton.icon(
+                    // push to another route
+                      onPressed: (){
+                        Navigator.pushNamed(context, '/locations');
+                      },
+                      icon: Icon(Icons.edit_location_alt,
+                          color: Colors.black),
+                      label: Text('Edit Location',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 23.0,
+                        fontFamily: 'Outfit',
+                        letterSpacing: 2.0,
+                        fontWeight: FontWeight.bold
+                      ),
+                      )),
+                  SizedBox(height: 20.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(data['location'],
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                        fontFamily: 'Outfit',
+                        fontWeight: FontWeight.bold
+                      )),
+                    ],
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(data['time'],
+                  style: TextStyle(
+                    fontSize: 66.0,
+                    fontFamily: 'Outfit'
+                  ),),
+                ],
+              ),
             ),
           )),
     );
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -6,6 +6,7 @@ class WorldTime {
   late String time;
   late String flag;
   late String url;
+  late bool isDayTime;
 
   WorldTime({required this.location, required this.flag, required this.url});
 
@@ -22,7 +23,10 @@ class WorldTime {
         DateTime now = DateTime.parse(datetime);
         now.add(Duration(hours: int.parse(offset)));
 
+
+        isDayTime = (now.hour > 6 && now.hour < 20) ? true : false;
         time = DateFormat.jm().format(now);
+
 
       } catch (e){
         time = 'could not fetch time data';
